074bd51a9789370938a17aa22b321f2484c5e7be
Mariapdv <maria.pereyra@davinci.edu.ar>
2026-06-27 19:17:02 -0300

    feat: estructura base del proyecto Spring Boot

---
+++ src/main/java/ar/edu/davinci/dv_ds_20261c_g29/DvDs20261cG29Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dv_ds_20261c_g29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20261cG29Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20261cG29Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dv_ds_20261c_g29/DvDs20261cG29ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dv_ds_20261c_g29;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20261cG29ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
